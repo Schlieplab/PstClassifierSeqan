a8e4f61766e3c5b94a393af9e1f6b0a9a010ae5b
Jan Qvick <jan.r.qvick@gmail.com>
2019-10-30 22:15:23 +0100

    Base for paralelization. Struggling with passing template function to thread.

--- src/search/lazy_suffix_tree/iteration.hpp
+++ src/search/lazy_suffix_tree/iteration.hpp
@@ -73,6 +73,74 @@ int node_occurrences(int node_index, std::vector<int> &table,
   }
   return occurrences;
 }
+bool label_valid(int label_start, int label_end) {
+  return true;
+}
+bool include_node(int label_start, int label_end, int count) {
+  int label_length = label_end - label_start;
+
+  return label_length < 15 && count >= 100 &&
+         label_valid(label_start, label_end);
+}
+bool staticFuctionCall(int node_index, int lcp, int edge_lcp,
+                       std::vector<int> &suffixes,
+                       std::vector<int> &table, std::vector<Flag> &flags){
+  int count = lst::details::node_occurrences(node_index, table, flags);
+
+  //this->counts.resize(this->table.size() / 2, -1);
+  //this->counts[node_index / 2] = count;
+
+  int label_start = table[node_index] - lcp;
+  int label_end = table[node_index] + edge_lcp;
+
+  //this->status.resize(this->table.size() / 2, Status::NONE);
+
+  if (include_node(label_start, label_end, count)) {
+    //this->status[node_index / 2] = Status::INCLUDED;
+    return true;
+  }
+  //this->status[node_index / 2] = Status::EXCLUDED;
+  return false;
+}
+
+
+template <seqan3::Alphabet alphabet_t>
+void BFIp(sequence_t<alphabet_t> &sequence,
+                                      std::vector<int> &suffixes,
+                                      std::vector<int> &table, std::vector<Flag> &flags,
+                                      bool expand_nodes, int start_node){
+
+  std::queue<std::tuple<int, int>> queue{};
+  iterate_children(start_node, table, flags,
+                   [&](int index) { queue.emplace(index, start_node); });
+
+  while (!queue.empty()) {
+    auto [node_index, lcp] = queue.front();
+
+    queue.pop();
+
+    int edge_lcp;
+
+    if (is_unevaluated(node_index, flags) && expand_nodes) {
+      edge_lcp = lst::details::expand_node(node_index, sequence, suffixes,
+                                           table, flags);
+    } else {
+      edge_lcp = get_edge_lcp(node_index, sequence, suffixes, table, flags);
+    }
+
+    bool consider_children = staticFuctionCall(node_index, lcp, edge_lcp, suffixes, table, flags);
+
+    if (!consider_children) {
+      continue;
+    }
+
+    int new_lcp = lcp + edge_lcp;
+    iterate_children(node_index, table, flags,
+                     [&](int index) { queue.emplace(index, new_lcp); });
+  }
+  seqan3::debug_stream << "TEST" << std::endl;
+}
+
 
 template <seqan3::Alphabet alphabet_t>
 void breadth_first_iteration(sequence_t<alphabet_t> &sequence,
@@ -81,6 +149,8 @@ void breadth_first_iteration(sequence_t<alphabet_t> &sequence,
                              const std::function<bool(int, int, int)> &f) {
   breadth_first_iteration(sequence, suffixes, table, flags, true, f);
 }
+
+
 
 template <seqan3::Alphabet alphabet_t>
 void breadth_first_iteration(sequence_t<alphabet_t> &sequence,
@@ -107,7 +177,7 @@ void breadth_first_iteration(sequence_t<alphabet_t> &sequence,
           aSuffix.push_back(j+1);
         }
         else if (letterCode == 1){
-          cSuffix.push_back(j+1);
+          cSuffix.push_back(j);
         }
         else if (letterCode == 2){
           gSuffix.push_back(j+1);
@@ -117,13 +187,23 @@ void breadth_first_iteration(sequence_t<alphabet_t> &sequence,
         }
       }
       for (int j : cSuffix) {
-        seqan3::debug_stream << suffixes[j] << std::endl;
+        seqan3::debug_stream << sequence[j] << std::endl;
       }
-      breadth_first_iteration_paralell(sequence, aSuffix, table, flags, true, 2, f);
-      breadth_first_iteration_paralell(sequence, cSuffix, table, flags, true, 4, f);
-      breadth_first_iteration_paralell(sequence, gSuffix, table, flags, true, 6, f);
-      breadth_first_iteration_paralell(sequence, tSuffix, table, flags, true, 8, f);
+      std::thread threads[4];
+      //threads[0] = std::thread(BFIp);
 
+      threads[0] = std::thread(&BFIp<int>, std::ref(sequence), std::ref(aSuffix), std::ref(table), std::ref(flags), true, 2);
+      //std::thread t1(BFIp, sequence, cSuffix, table, flags, true, 4, f);
+      //std::thread t2(BFIp, sequence, gSuffix, table, flags, true, 6, f);
+      //std::thread t3(BFIp, sequence, tSuffix, table, flags, true, 8, f);
+
+      //threads[1] = t1;
+      //threads[2] = t2;
+      //threads[3] = t3;
+      for (int i = 0; i < 4; ++i) {
+        threads[i].join();
+      }
+      getchar();
       break;
     }
     queue.pop();
@@ -147,43 +227,6 @@ void breadth_first_iteration(sequence_t<alphabet_t> &sequence,
     iterate_children(node_index, table, flags,
                      [&](int index) { queue.emplace(index, new_lcp); });
   }
-}
-
-template <seqan3::Alphabet alphabet_t>
-void breadth_first_iteration_paralell(sequence_t<alphabet_t> &sequence,
-                             std::vector<int> &suffixes,
-                             std::vector<int> &table, std::vector<Flag> &flags,
-                             bool expand_nodes, int start_node,
-                             const std::function<bool(int, int, int)> &f){
-
-    std::queue<std::tuple<int, int>> queue{};
-    iterate_children(start_node, table, flags,
-                     [&](int index) { queue.emplace(index, start_node); });
-
-    while (!queue.empty()) {
-        auto [node_index, lcp] = queue.front();
-
-        queue.pop();
-
-        int edge_lcp;
-
-        if (is_unevaluated(node_index, flags) && expand_nodes) {
-            edge_lcp = lst::details::expand_node(node_index, sequence, suffixes,
-                                                 table, flags);
-        } else {
-            edge_lcp = get_edge_lcp(node_index, sequence, suffixes, table, flags);
-        }
-
-        bool consider_children = f(node_index, lcp, edge_lcp);
-
-        if (!consider_children) {
-            continue;
-        }
-
-        int new_lcp = lcp + edge_lcp;
-        iterate_children(node_index, table, flags,
-                         [&](int index) { queue.emplace(index, new_lcp); });
-    }
 }
 
 
