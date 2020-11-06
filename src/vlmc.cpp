74edd4308c5c1a0c904c28b3511e6c7d40c2feb0
Joel Gustafsson <me@joelgustafsson.com>
2020-11-06 16:01:44 +0100

    Add train cpp wrapper for cython

--- src/vlmc.cpp
+++ src/vlmc.cpp
@@ -19,6 +19,22 @@ lst::details::sequence_t<seqan3::dna5> to_dna(const char *sequence_) {
   return sequence_as_dna;
 }
 
+std::string train_kl_cpp(const std::string &id_, const std::string &sequence_,
+                         const size_t max_depth, const size_t min_count,
+                         const float threshold, const bool multi_core,
+                         const int parallel_depth) {
+  auto sequence_as_dna =
+      sequence_ | seqan3::views::char_to<seqan3::dna5> |
+      seqan3::views::to<lst::details::sequence_t<seqan3::dna5>>;
+
+  pst::KullbackLieblerTreeMap<seqan3::dna5> pst{
+      id_,       sequence_as_dna, max_depth,     min_count,
+      threshold, multi_core,      parallel_depth};
+  pst.construct_tree();
+
+  return pst.to_tree();
+}
+
 extern "C" {
 static const char *train_kl(const char *id_, const char *sequence_,
                             size_t max_depth, size_t min_count, float threshold,
