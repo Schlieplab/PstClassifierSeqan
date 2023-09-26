e9c300e4dd4ec7f8204d050d3b93ee2c0f46a275
Joel Gustafsson <me@joelgustafsson.com>
2023-09-26 13:46:42 +0200

    Remove unused files in python package

--- python-package/libvlmc/__init__.py
+++ python-package/libvlmc/__init__.py
@@ -1,2 +1,1 @@
 from ._libvlmc import train, score_sequences_cython, score_sequences, score_sequences_background, sliding_windows, sliding_windows_background, sliding_windows_cython, sliding_windows_background_cython, d2, d2star, cv, dvstar
-#from .KullbackLieblerTreeMap import pyKullbackLieblerTreeMap
